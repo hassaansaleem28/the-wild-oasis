b3ebf483a69e3f1cf982204796da8d6f995255d3
hassaansaleem28 <iamhassaans@gmail.com>
2025-06-06 15:41:14 +0500

    update AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./header";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
@@ -9,11 +9,13 @@ const StyledAppLayout = styled.div`
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
+
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
 `;
+
 const Container = styled.div`
   max-width: 120rem;
   margin: 0 auto;
@@ -21,6 +23,7 @@ const Container = styled.div`
   flex-direction: column;
   gap: 3.2rem;
 `;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
